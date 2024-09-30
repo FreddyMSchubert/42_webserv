7f8a89ce1441ac869c6a6fdff6a021106b39b8c9
jkauker <jkauker@student.42heilbronn.de>
2024-09-30 15:10:35 +0200

    basic files and structure

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "../include/Webserv.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+		return 1;
+	}
+}
