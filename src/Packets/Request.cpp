6fd2030a07b348a5dfba907fa6f5345082edc56c
Jonas <jonas.kauker2003@gmail.com>
2024-10-10 15:22:44 +0200

    added idea

--- src/Packets/Request.cpp
+++ src/Packets/Request.cpp
@@ -1,6 +1,14 @@
 #include "../../include/Packets/Request.hpp"
 
+// TODO: this function should not return void but the response to the request that we can send it back directly
 void Request::Run()
 {
 	std::cout << "Request::Run()" << std::endl;
 }
+
+/*
+Response Request::Run()
+{
+	// do what the request is supposed to do and then return the response
+}
+*/
