4610f6c88d9ea758d6591fa46cdbfaa3acc069e2
Jonas <jonas.kauker2003@gmail.com>
2024-10-14 19:47:04 +0200

    added comment on how to handle directory requests

--- src/Packets/Methods/Get.cpp
+++ src/Packets/Methods/Get.cpp
@@ -1,8 +1,8 @@
 #include "../../../include/Packets/Request.hpp"
 
-void Request::handleGet(t_server_config &config, Response &response)
+static void handle_file_req(t_server_config &config, Response &response, Request &req)
 {
-	std::string path = getPath();
+	std::string path = req.getPath();
 	if (path == std::string("/"))
 		response.setPath(config.default_location.index); // TODO: i think we should loop trough all the paths and pick the first one to exist
 	else
@@ -14,7 +14,6 @@ void Request::handleGet(t_server_config &config, Response &response)
 
 	try
 	{
-		// TODO: something here is wrong
 		std::string file = getFileAsString(std::string(config.default_location.root) + path);
 		response.addHeader("Content-Length", std::to_string(file.size()));
 		response.setStatus(Status::OK);
@@ -27,4 +26,32 @@ void Request::handleGet(t_server_config &config, Response &response)
 		response = Response(); // TODO: do the actual error handling here
 		response.setStatus(Status::NotFound);
 	}
+}
+
+static void handle_dir_req(t_server_config &config, Response &response, Request &req)
+{
+	(void)config;
+	(void)response;
+	(void)req;
+
+	// if path is /, we should return the index file if there is one and if not, we should return a list of files in the directory
+	// and if the directory does not exist we return 404
+}
+
+static bool is_file_req(Request &req)
+{
+	std::string path = req.getPath();
+	if (path[path.length() - 1] == '/') // XXX: dont know what they mean with /$ at the end of the path.
+		return false;
+	return true;
+}
+
+void Request::handleGet(t_server_config &config, Response &response)
+{
+
+	if (is_file_req(*this))
+		handle_file_req(config, response, *this);
+	else
+		handle_file_req(config, response, *this);
+	
 }
