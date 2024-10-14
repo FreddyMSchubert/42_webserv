99dcf8f31fbb9960d8acf53f5748883860f8d01b
jkauker <jkauker@student.42heilbronn.de>
2024-10-14 09:42:47 +0200

    added tetris

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,40 @@
 
 std::vector<t_server_config> init_testing_configs()
 {
-	std::vector<t_server_config> configs(2);
+	std::vector<t_server_config> configs(3);
 
 	configs[0].server_names.push_back("clickergame.com");
 	configs[1].server_names.push_back("platformergame.com");
+	configs[2].server_names.push_back("teeetris.com");
 
 	configs[0].host = "127.0.0.1";
 	configs[1].host = "127.0.0.1";
+	configs[2].host = "127.0.0.1";
 
 	configs[0].port = 8080;
 	configs[1].port = 8081;
+	configs[2].port = 4242;
 
 	configs[0].default_location.allowed_methods.push_back(Method::GET);
 	configs[1].default_location.allowed_methods.push_back(Method::GET);
 	configs[1].default_location.allowed_methods.push_back(Method::POST);
+	configs[2].default_location.allowed_methods.push_back(Method::GET);
 
 	configs[0].default_location.root = "./www/clicker";
 	configs[1].default_location.root = "./www/platformer";
+	configs[2].default_location.root = "./www/tetris";
 
 	configs[0].default_location.index = "/index.html";
 	configs[1].default_location.index = "/index.html";
+	configs[2].default_location.index = "/index.html";
 
 	configs[0].default_location.directory_listing = false;
 	configs[1].default_location.directory_listing = true;
+	configs[2].default_location.directory_listing = false;
 
 	configs[0].default_location.client_max_body_size = 1000000;
 	configs[1].default_location.client_max_body_size = 1000;
+	configs[2].default_location.client_max_body_size = 1000000;
 
 	configs[1].error_pages.push_back((t_error_page){404, (t_location){std::vector<Method>(), "./www/platformer/404", "404.html", false, 0}});
 
