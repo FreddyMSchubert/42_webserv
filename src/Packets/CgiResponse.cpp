e0a59e5e32312cc8552b62e3518e07082753cc0d
FreddyMSchubert <github@frederickschubert.de>
2024-12-16 11:38:03 +0100

    execution command determination

--- src/Packets/CgiResponse.cpp
+++ src/Packets/CgiResponse.cpp
@@ -2,5 +2,21 @@
 
 void Response::handleCgiResponse(Request &req, Config &config)
 {
-	Path path(req.getPath(), Path::Type::URL, config);
+	FilePath fpath(req.getPath(), Path::Type::URL, config);
+	std::string extension = fpath.getFileExtension();
+	std::string exec_cmd;
+
+	std::vector<t_location> locations = get_locations(config, req.getPath());
+	for (t_location loc : locations)
+	{
+		std::vector<std::pair<std::string, std::string>> cgi_extensions = loc.cgi_extensions;
+		for (std::pair<std::string, std::string> ext : cgi_extensions)
+		{
+			if (ext.first == extension)
+			{
+				exec_cmd = ext.second;
+				break;
+			}
+		}
+	}
 }
